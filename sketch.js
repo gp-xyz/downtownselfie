3f4f5ad21596fc331711cc925e72c840ba3310dd
gp-xyz <102692598+gp-xyz@users.noreply.github.com>
2022-04-02 19:28:15 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ var counter=0;
 function setup() {
   createCanvas(500, 600);
   var daymode = random(['night','day']);
-  fremont = new Street(daymode,3,6,width/10);
+  fremont = new Street(daymode,random([1,3,10]),random([4,6,10]),width/10);
 
 }
 
